cc69a52b06a02d9ca407f74e7ea6d51828673143
Samantha Zhang <i@samanthaz.me>
2014-09-21 20:32:31 -0700

    Part 2

--- js/blog.js
+++ js/blog.js
@@ -3,12 +3,30 @@ $(function() {
 	Parse.$ = jQuery;
 
 	// Replace this line with the one on your Quickstart Guide Page
-	Parse.initialize("W8vTW6MTre3g0ScTeyPzqc6Uzj2KZoQ6GBv0j6ZC", "VVayP3EdZ6QH0QMttzpWgeJ2if4f2m8QjA10SaFQ");
+	Parse.initialize("HC87tn6aA7c3sYx9X0vwwLVXeqHDRMYYmrUBK5zv", "3piNGGnRMhvWo8u9pKD9TDc1MJlWhlvK78Vr3fHo");
 
-	var TestObject = Parse.Object.extend("TestObject");
-	var testObject = new TestObject();
-	testObject.save({foo: "bar"}).then(function(object) {
-	  alert("yay! it worked");
-	});
+	var Blog = Parse.Object.extend("Blog"),
+		Blogs = Parse.Collection.extend({
+			model: Blog
+		}),
+		BlogsView = Parse.View.extend({
+			template: Handlebars.compile($('#blogs-tpl').html()),
+			render: function() { 
+				var collection = { blog: this.collection.toJSON() };
+				this.$el.html(this.template(collection));
+			}
+		});
+		blogs = new Blogs();
+
+		blogs.fetch({
+			success: function(blogs) {
+				var blogsView = new BlogsView({ collection: blogs });
+				blogsView.render();
+				$('.main-container').html(blogsView.el);
+			},
+			error: function(blogs, error) {
+				console.log(error);
+			}
+		});
 
 });
